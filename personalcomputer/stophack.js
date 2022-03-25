f6f451c55c69f3f5cea0af4e8093d52b66e14b8d
grexcode <79855427+grexcode@users.noreply.github.com>
2022-03-25 08:47:16 -0400

    Create stophack.js

---
+++ personalcomputer/stophack.js
@@ -0,0 +1,17 @@
+
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "nybergk" && password === "june1817") {
+        alert("You have successfully logged in.");
+        location.href = "https://grexcode.github.io/marsisdead/personalcomputer/desktop.html";
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
