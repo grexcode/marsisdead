ed42216b29f89c67b18046f9476817dad52cd77f
grexcode <79855427+grexcode@users.noreply.github.com>
2022-03-28 10:31:51 -0400

    Create endgame.js

---
+++ personalcomputer/endgame.js
@@ -0,0 +1,29 @@
+
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const password = loginForm.password.value;
+
+    if (password === "marsisdead") {
+        alert("You have successfully logged in.");
+        location.href = "https://grexcode.github.io/marsisdead/personalcomputer/thefinale.html";
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
+
+ function JavaBlink() {
+     var blinks = document.getElementsByTagName('JavaBlink');
+     for (var i = blinks.length - 1; i >= 0; i--) {
+        var s = blinks[i];
+        s.style.visibility = (s.style.visibility === 'visible') ? 'hidden' : 'visible';
+     }
+     window.setTimeout(JavaBlink, 800);
+  }
+  if (document.addEventListener) document.addEventListener("DOMContentLoaded", JavaBlink, false);
+  else if (window.addEventListener) window.addEventListener("load", JavaBlink, false);
+  else if (window.attachEvent) window.attachEvent("onload", JavaBlink);
+  else window.onload = JavaBlink;
