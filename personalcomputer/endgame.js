80898e9e907b3948b2f15cce286805fe6163b2af
grexcode <79855427+grexcode@users.noreply.github.com>
2022-03-28 16:49:11 -0400

    Update endgame.js

--- personalcomputer/endgame.js
+++ personalcomputer/endgame.js
@@ -7,7 +7,7 @@ loginButton.addEventListener("click", (e) => {
     e.preventDefault();
     const password = loginForm.password.value;
 
-    if (password === "marsisdead") {
+    if (password === "mars is dead") {
         alert("You have stopped the hack :(");
         location.href = "https://grexcode.github.io/marsisdead/personalcomputer/thefinale.html";
     } else {
