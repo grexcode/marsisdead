40002c90b54790d9b47f53aa7815267386249c5f
grexcode <79855427+grexcode@users.noreply.github.com>
2022-04-11 10:53:07 -0400

    Update home.js

--- commandcenter/home.js
+++ commandcenter/home.js
@@ -5,14 +5,25 @@ const loginErrorMsg = document.getElementById("login-error-msg");
 
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
-    const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (password === "sofia") {
+    if (password === "SOFIA") {
         alert("You have successfully logged in.");
         location.href = "https://grexcode.github.io/marsisdead/commandcenter/success.html";
-    } 
-    else {
+    } else {
         loginErrorMsg.style.opacity = 1;
     }
-}
+})
+
+ function JavaBlink() {
+     var blinks = document.getElementsByTagName('JavaBlink');
+     for (var i = blinks.length - 1; i >= 0; i--) {
+        var s = blinks[i];
+        s.style.visibility = (s.style.visibility === 'visible') ? 'hidden' : 'visible';
+     }
+     window.setTimeout(JavaBlink, 800);
+  }
+  if (document.addEventListener) document.addEventListener("DOMContentLoaded", JavaBlink, false);
+  else if (window.addEventListener) window.addEventListener("load", JavaBlink, false);
+  else if (window.attachEvent) window.attachEvent("onload", JavaBlink);
+  else window.onload = JavaBlink;
